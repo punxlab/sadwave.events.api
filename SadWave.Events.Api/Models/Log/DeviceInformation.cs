de5fd37c275379580908ac3c54048f8a700c34bf
Павел <kashirin.p.v@gmail.com>
2020-08-26 23:12:09 +0300

    Add ignored files

---
+++ SadWave.Events.Api/Models/Log/DeviceInformation.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace SadWave.Events.Api.Models.Log
+{
+	public class DeviceInformation
+	{
+		[JsonProperty("model")]
+		public string Model { get; set; }
+
+		[JsonProperty("manufacturer")]
+		public string Manufacturer { get; set; }
+
+		[JsonProperty("version")]
+		public string Version { get; set; }
+
+		[JsonProperty("platform")]
+		public string Platform { get; set; }
+	}
+}
---
+++ SadWave.Events.Api/Models/Log/ErrorLogMessage.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace SadWave.Events.Api.Models.Log
+{
+	public class ErrorLogMessage
+	{
+		[Required]
+		[JsonProperty("message")]
+		public string Message { get; set; }
+
+		[JsonProperty("exception")]
+		public string Exception { get; set; }
+
+		[JsonProperty("tag")]
+		public string Tag { get; set; }
+
+		[JsonProperty("device")]
+		public DeviceInformation DeviceInformation { get; set; }
+	}
+}
